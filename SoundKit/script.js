32e110ec6ca3a0db34e02c982bafde64e8052d19
davidwzz <dawid.jan.wozniak@gmail.com>
2019-03-12 19:47:37 +0100

    Added: playing audio on key down event

--- SoundKit/script.js
+++ SoundKit/script.js
@@ -2,3 +2,11 @@ function playSample (sampleName) {
     var sample = document.getElementById(sampleName);
     sample.play();
 }
+
+document.addEventListener('keydown', function (e) {
+    
+        var samplex = document.getElementById(e.key);
+        samplex.click();
+})
+
+
